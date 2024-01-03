637bcd50b544cd8f588207f9225fff1f11998142
wal-idwow <medali25tech@gmail.com>
2024-01-03 18:59:51 +0100

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,9 @@ def is_lower(c):
         return True
     else:
         return False
+
 def uppercase(s):
     for char in s:
         print("{:c}".format(ord(char) if not is_lower(char) else ord(char) - 32)
-              , end="")
+            , end="")
     print("")
