48181e24311251399b56753674cad21647b7d200
wal-idwow <medali25tech@gmail.com>
2024-01-03 14:11:38 +0100

    first commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:
+        #check of the char is a lowercase letter
+        if 'a' <= char <= 'z':
+            #convertision of lower to upper
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+            print(uppercase_char, end='')
+        else:
+            print(char, end='')
+    #print newline
+    print()
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+this is python if_else_loops_functions README
