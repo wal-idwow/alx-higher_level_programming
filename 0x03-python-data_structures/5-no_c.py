f44c7c9d2e2d15aa5ed1660754a185bd5b56061a
wal-idwow <medali25tech@gmail.com>
2024-01-08 00:28:26 +0100

    done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,4 @@ def no_c(my_string):
     for num in range(len(my_string)):
         if (my_string[num] != 'C' and my_string[num] != 'c'):
             fn += my_string[num]
-    return fn 
-        
+    return fn      
