ac0faf6da9e4ad44bd514a34329166d68a19bae8
wal-idwow <medali25tech@gmail.com>
2024-01-08 12:30:58 +0100

    doneclear

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    # Check if sentence is empty
     if not sentence:
         return (0, None)
     
-    # Return a tuple with the string length and the first character
     return (len(sentence), sentence[0])
