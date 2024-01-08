ca313213401fc716fc9aee17d22ab5f757b03034
wal-idwow <medali25tech@gmail.com>
2024-01-08 15:39:19 +0100

    done

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,55 +7,49 @@
  * @head: pointer to head of the list
  * Return: 1 if the list is a palindrome, 0 if not
 */
-// check if the list is empty or has only element
+
+/*check if the list is empty or has only element*/
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
+	if (*head == NULL || (*head)->next == NULL)
+	return (1);
 
-    // declare pointers for traversting the list
-    listint_t *onestp = *head, *twostps = *head, *bfrone = NULL, *midl = NULL;
-    int is_palindrome = 1;
+	/* declare pointers for traversting the list*/
+	listint_t *onestp = *head, *twostps = *head, *bfrone = NULL, *midl = NULL;
+	int is_palindrome = 1;
 
-    // move twostps twice as fast as onestp to find the middle of the list
-    while (twostps != NULL && onestp->next != NULL)
-    {
-        twostps = twostps->next->next;
+	while (twostps != NULL && onestp->next != NULL)
+	{
+		twostps = twostps->next->next;
+		bfrone = onestp;
+		onestp = onestp->next;
+	}
 
-        bfrone = onestp;
-        onestp = onestp->next;
-    }
+	if (twostps != NULL)
+	{
+		midl = onestp;
+		onestp = onestp->next;
+	}
 
-    if (twostps != NULL)
-    {
-        midl = onestp;
-        onestp = onestp->next;
-    }
+	listint_t *second_half = reverse_list(onestp);
+	listint_t *first_half = *head;
 
-    // reverse the second half of the list
-    listint_t *second_half = reverse_list(onestp);
-    listint_t *first_half = *head;
+	while (seconid_half != NULL)
+	{
+		if (first_half->n != second_half->n)
+	{
+		is_palindrome = 0;
+		break;
+	}
 
-    // compare elements of the first and second halves
-    while (second_half != NULL)
-    {
-        if (first_half->n != second_half->n)
-        {
-            is_palindrome = 0;
-            break;
-        }
+		first_half = first_half->next;
+		second_half = second_half->next;
+	}
 
-        first_half = first_half->next;
-        second_half = second_half->next;
-    }
+	reverse_list(midl);
+	bfrone->next = midl;
 
-    // reverse the second half back to its original order
-    reverse_list(midl);
-    // connect the reversed second half to the first half
-    bfrone->next = midl;
-
-    return is_palindrome;
-       
+	return (is_palindrome);
 }
 
 /**
@@ -66,15 +60,14 @@ int is_palindrome(listint_t **head)
 
 listint_t *reverse_list(listint_t *head)
 {
-    listint_t *prv = NULL, *current = head, *next;
+	listint_t *prv = NULL, *current = head, *next;
 
-    // reverse the list by changing the dirction of pointers
-    while (current != NULL)
-    {
-        next = current->next;
-        current->next = prv;
-        prv = current;
-        current = next;
-    }
-    return prv;
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prv;
+		prv = current;
+		current = next;
+	}
+	return (prv);
 }
