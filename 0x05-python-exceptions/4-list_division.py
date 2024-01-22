b75bfe6b2fbff1fac57a53cd90cd8db5759bcb66
wal-idwow <medali25tech@gmail.com>
2024-01-22 22:46:13 +0100

    mandatory

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+  
     results = []
     i = 0
-    
+
     for i in range(list_length):
         try:
             operation = my_list_1[i] / my_list_2[i]
