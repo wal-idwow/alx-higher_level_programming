2da7bcb98c1c6ca67b805579dcca9f8ca9e96619
wal-idwow <medali25tech@gmail.com>
2024-01-23 20:32:44 +0100

    mandatory

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 """Defines a class Square"""
+
+
 class Square:
+    """class  define square properties by: (based on 0-square.py)
+    
+    Attribute:
+        size: square size
+    """
     def __init__(self, size):
+        """new square instance 
+        
+        Attribute:
+            size: square size"""
         self.__size = size
