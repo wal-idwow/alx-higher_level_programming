bf518e4455a059d1e0a220f1029600887077f4b8
wal-idwow <medali25tech@gmail.com>
2024-01-30 20:32:56 +0100

    mandatory

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def copy_list(l):
-    """Creates a shallow copy of the given list
-
-    Args:
-        l (list): The list to be copied
-
-    Returns:
-        list: A shallow copy of the input list
-    """
     return l.copy()
