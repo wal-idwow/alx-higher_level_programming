4aabf38749e6a513c87395fb73fc6919783ad034
wal-idwow <medali25tech@gmail.com>
2024-01-31 02:03:28 +0100

    mandatory

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(i=0):
     magic_string.i = getattr(magic_string, "i", 0) + 1
-    return((("Holberton, " * magic_string.i))[:-2])
+    return((("BestSchool, " * magic_string.i))[:-2])
