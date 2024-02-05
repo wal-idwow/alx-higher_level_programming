c26bbb19a1e363e573c95d19fa0ae253d89d8870
wal-idwow <medali25tech@gmail.com>
2024-02-05 19:36:30 +0100

    mandatory

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""method lookup module
+"""
+
+
+def lookup(obj):
+    """fonction that return the attributes and methods of an object
+
+    Args:
+        obj (object): object for which attributes and methods are to be looked up
+    """
+    return dir(obj)
