e7c085f2136c85f1fb81a0bbd24ac2100678dc0e
wal-idwow <medali25tech@gmail.com>
2024-02-07 01:09:17 +0100

    advanced

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int."""
+"""Defines class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """Invert int operators == and !=."""
+    """invert int operators == and !=."""
 
     def is_equal(self, other):
-        """Override == operator with != behavior."""
+        """override == operator with != behavior."""
         return super().__ne__(other)
 
     def is_not(self, other):
-        """Override != operator with == behavior."""
+        """override != operator with == behavior."""
         return super().__eq__(other)
