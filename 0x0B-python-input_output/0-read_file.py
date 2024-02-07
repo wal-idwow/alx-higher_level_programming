79833de691f53356250af934371d6b8a435713d8
wal-idwow <medali25tech@gmail.com>
2024-02-07 12:59:31 +0100

    mandatory

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """define a function that reads a text file (UTF8) and prints it to stdout"""
 
+
 def read_file(filename=""):
     """print the content of a UTF8 text file to stdout
 
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""define function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""define function that writes a string to a text file (UTF8)"""
 
 
 def write_file(filename="", text=""):
