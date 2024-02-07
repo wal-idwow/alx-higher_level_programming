05d860612897e4faca110a25dc11f11631516140
wal-idwow <medali25tech@gmail.com>
2024-02-07 21:56:14 +0100

    mandatory

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    """ Pascals triangle of n"""
-    if n <= 0:
-        return []
-
+    """Pascals triangle of n"""
+    lists = []
     triangle = [[1]]
 
+    if n <= 0:
+        return lists
     for x in range(1, n):
         row = [1]
-
         for i in range(1, x):
             row.append(triangle[x-1][i-1] + triangle[x-1][i])
-
         row.append(1)
         triangle.append(row)
 
