66dc8c1e32bbdfeb6cf25e9e70484e3de6ebd251
wal-idwow <medali25tech@gmail.com>
2024-02-13 03:16:09 +0100

    S/R/B

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,7 +14,7 @@ class Square(Rectangle):
     def __str__(self):
         """str method"""
         first_str = "[Square]"
-        second_str = "({}) ".format(self. id)
+        second_str = " ({}) ".format(self. id)
         third_str = "{}/{} - ".format(self.x, self.y)
         fourth_str = "{}/{}".format(self.width, self.height)
 
@@ -31,20 +31,11 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """str method"""
-        first_str = "[Square]"
-        second_str = " ({}) ".format(self.id)
-        third_str = "{}/{} - ".format(self.x, self.y)
-        fourth_str = "{}".format(self.size)
-
-        return first_str + second_str + third_str + fourth_str
-
     def update(self, *args, **kwargs):
         """method update"""
         if args is not None and len(args) != 0:
             attr = ['id', 'size', 'x', 'y']
-            for i in range(len(args)):
+            for i in (len(args)):
                 if attr[i] == 'size':
                     setattr(self, 'width', args[i])
                     setattr(self, 'height', args[i])
@@ -57,7 +48,6 @@ class Square(Rectangle):
                     setattr(self, 'height', value)
                 else:
                     setattr(self, key, value)
-
 
     def to_dictionary(self):
         """returns dictioanry with his attributes"""
