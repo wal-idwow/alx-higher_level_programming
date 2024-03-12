5cecde02dea01860a4ef83990a5c05f71a9d3171
wal-idwow <medali25tech@gmail.com>
2024-03-12 23:31:17 +0100

    push SQL introduction tasks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---  creates a table second_table in the database hbtn_0c_0 in my MySQL server and add multiples rows.
+--  creates a table second_table in the database .
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
