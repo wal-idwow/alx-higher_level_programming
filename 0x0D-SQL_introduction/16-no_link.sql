bdb3e03863bc104c658ce695eb0ee4c48829f395
wal-idwow <medali25tech@gmail.com>
2024-03-12 23:14:58 +0100

    push SQL introduction tasks

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 --  lists all records of the table second_table of the database hbtn_0c_0
 --  listed by descending score
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' != ""
-ORDER BY 'score' DESC
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
