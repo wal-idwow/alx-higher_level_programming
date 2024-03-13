6e6b8eb9e6bc1c475368c3c500506687a715cbc4
wal-idwow <medali25tech@gmail.com>
2024-03-13 14:13:09 +0100

    push more queries tasks

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,9 +2,7 @@
 CREATE DATABASE
     IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost';
+   ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
