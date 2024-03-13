665c603c7e3d69d60af141d183d4d6e92c70c802
wal-idwow <medali25tech@gmail.com>
2024-03-13 14:07:22 +0100

    push more queries tasks

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,5 @@ CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    ON * . * TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1'@'localhost';
+    ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
