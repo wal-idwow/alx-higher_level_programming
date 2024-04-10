5eef4e0ce547e423d4a3c13fac1703f4ec687189
wal-idwow <medali25tech@gmail.com>
2024-04-10 01:06:48 +0000

    more fixes

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (h, w) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/README.md
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -2,3 +2,8 @@
 
   - Tasks:
     * 0. Rectangle #0
+    * 1. Rectangle #1
+    * 2. Rectangle #2
+    * 3. Rectangle #3
+    * 4. Rectangle #4
+    * 5. Square #0
