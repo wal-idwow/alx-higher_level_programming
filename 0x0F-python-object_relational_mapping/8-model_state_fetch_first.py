559ab71361342cfc757ab587515807781fc53c4a
wal-idwow <medali25tech@gmail.com>
2024-04-14 13:40:42 +0000

    fix error + 10

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""prints the first State object from the database"""
+"""List the State object from hbtn_0e_6_usa db"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import State, Base
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
+    eng = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
-
-    session = Session(eng)
-    one = session.query(State).order_by(State.id).one()
-    if one:
-        print("{}: {}".format(one.id, one.name))
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
     session.close()
