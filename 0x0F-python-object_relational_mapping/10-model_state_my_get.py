169625f8c1b9612c2f7aa2a2a28b43e7eb9b00ad
wal-idwow <medali25tech@gmail.com>
2024-04-14 13:26:14 +0000

    fix error + 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed
-as argument from hbtn_0e_6_usa db"""
+"""Prints the State object with the name passed
+as an argument from hbtn_0e_6_usa db"""
 
 if __name__ == "__main__":
-
     import sys
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm.exc import NoResultFound
     from model_state import Base, State
 
     eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                         .format(sys.argv[1], sys.argv[2],
                                 sys.argv[3]), pool_pre_ping=True)
-    Base.metedata.create_all(eng)
 
-    session = Session(eng)
-    state = session.query(State).filter(State.name == sys.argv[4]).one()
-    if state:
+    Base.metadata.create_all(eng)
+
+    Session = sessionmaker(bind=eng)
+    session = Session()
+
+    try:
+        state = session.query(State).filter(State.name == sys.argv[4]).one()
         print("{}".format(state.id))
-    else:
+    except NoResultFound:
         print("Not found")
+
     session.close()
