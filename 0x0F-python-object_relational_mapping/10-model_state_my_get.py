5a8bd81eaf8e6a6b2d1d4e3af79b146608cea82f
wal-idwow <medali25tech@gmail.com>
2024-04-14 13:34:39 +0000

    fix error + 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed
-as argument from hbtn_0e_6_usa db"""
+"""prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
-    import sys
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from model_state import Base, State
-
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(sys.argv[1], sys.argv[2],
-                                sys.argv[3]), pool_pre_ping=True)
-    Base.metedata.create_all(eng)
-
-    session = Session(eng)
-    state = session.query(State).filter(State.name == sys.argv[4]).one()
+    eng = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    state = session.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
     if state:
-        print("{}".format(state.id))
+        print("{}".format(state[0].id))
     else:
         print("Not found")
     session.close()
