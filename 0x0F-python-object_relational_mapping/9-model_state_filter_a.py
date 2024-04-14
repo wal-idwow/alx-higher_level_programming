2bc523005ed5fae4d9536e508bec4206924a3b02
wal-idwow <medali25tech@gmail.com>
2024-04-14 17:12:40 +0000

    fix error + 9,10

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """lists all State objects that contain the letter a from hbtn_0e_6_usa db"""
 from sys import argv
+from sqlalchemy import create_engine
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     eng = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
         .format(argv[1], argv[2],
                 argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(eng)
